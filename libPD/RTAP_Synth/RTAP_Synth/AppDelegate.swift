51109b56ff89bedd489d33d46fbb116680888f00
thomaschhh <holz.thomas@yahoo.de>
2020-09-11 13:26:45 +0200

    replaced the TouchOSC Patch, added AudioKit and libPD

---
+++ libPD/RTAP_Synth/RTAP_Synth/AppDelegate.swift
@@ -0,0 +1,54 @@
+//
+//  AppDelegate.swift
+//  RTAP_Synth
+//
+//  Created by Thomas Holz on 10.09.20.
+//  Copyright © 2020 Thomas Holz. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow? // this wasn't here before  # Thomas
+    var pd:PdAudioController? // initialize the PD audio controller # Thomas
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        // Create an instance of the PdAudioController # Thomas
+        pd = PdAudioController()
+        
+        // Start the pd instance and get a status update # Thomas
+        let pdInit = pd?.configureAmbientWithSampleRate(44100, numberChannels: 2, mixingEnabled: true)
+        
+        if pdInit == PdAudioO{
+            print("Pd is ready to go.")
+        } else {
+            print("Something might be wrong with PD.")
+        }
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        
+        // Stop the pd engine when the app closes # Thomas
+        // pd?.active = false
+        
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ libPD/RTAP_Synth/RTAP_Synth/RTAP_Synth-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  RTAP_Synth-Bridging-Header.h
+//  RTAP_Synth
+//
+//  Created by Thomas Holz on 10.09.20.
+//  Copyright © 2020 Thomas Holz. All rights reserved.
+//
+
+#ifndef RTAP_Synth_Bridging_Header_h
+#define RTAP_Synth_Bridging_Header_h
+
+# import "PdAudioController.h"
+# import "PdDispatcher.h"
+# import "PDPatch.h"
+
+#endif /* RTAP_Synth_Bridging_Header_h */
